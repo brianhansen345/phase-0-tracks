877715972420a0dab47f631aad076cbacb319a9e
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-14 13:13:19 -0700

    Add More to Assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,16 +5,18 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
+"iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".insert(-1, "    ").insert(0, "    ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase!
 # => "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
