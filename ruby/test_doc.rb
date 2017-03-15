7188779730887e7f09dea45e15da0a2f6a345da2
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-15 10:10:55 -0700

    New Commit

---
+++ ruby/test_doc.rb
@@ -0,0 +1,11 @@
+def adder(x,y)
+	sum = x + y
+	p sum
+end
+
+def adder_then_multiplier(a,b,c)
+	product = adder(x,y) * z
+	p product
+end
+
+adder_then_multiplier(1,2,3)
---
+++ ruby/text_ruby.rb
@@ -0,0 +1,6 @@
+def add(x,y)
+  sum = x + y
+  p "Sum is #{sum}"
+end
+
+add(2,3)
