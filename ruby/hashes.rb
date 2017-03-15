3384a2f7c5e468ea5480beaaa14ea0293b89b763
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-15 14:09:07 -0700

    Complete Assignment

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,20 +1,24 @@
+# Establish blank values for needed symbols in array
+
 data = {
-	:name = "",
-	:age = "",
-	:num_children = "",
-	:decor_theme = "",
-	:loves_all_black? = "",
-	:money_no_object? = "",
+	:name => "",
+	:age => "",
+	:num_children => "",
+	:decor_theme => "",
+	:loves_all_black => "",
+	:money_no_object => "",
 }
+
+# Get required input from user to assign actual values to array symbols.
 
 puts "What's your name?"
 data[:name] = gets.chomp
 
 puts "What's your age?"
-data[:age] = gets.chomp
+data[:age] = gets.chomp.to_i
 
 puts "How many children do you have?"
-data[:num_children] = gets.chomp
+data[:num_children] = gets.chomp.to_i
 
 puts "What do you want your decor theme to be?"
 data[:decor_theme] = gets.chomp
@@ -25,11 +29,13 @@ data[:loves_all_black] = gets.chomp
 puts "Is money no object?"
 data[:money_no_object] = gets.chomp
 
+# Allow user to edit one value.
+
 puts "Do you want to update any key? If so, which one? If not, enter 'none'."
 update = gets.chomp.downcase
-	break if update == "none"
-puts "What would you like the new value to be?"
-update2 = gets.chomp.downcase
+if update != "none"
+	puts "What would you like the new value to be?"
+	update2 = gets.chomp.downcase
 	if update == "name"
 		data[:name] = update2
 	end
@@ -48,9 +54,11 @@ update2 = gets.chomp.downcase
 	if update == "money no object"
 		data[:money_no_object] = update2
 	end
+end
+
+# Print the array.
 
 p data
-
 
  # (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.)
  # HOW TO USE THE ABOVE???
