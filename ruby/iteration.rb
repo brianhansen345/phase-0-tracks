3140b08aef140218497828983ea5186b38a4572d
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-16 11:23:47 -0700

    Add More Data

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,34 +1,23 @@
-array = ["Red", "Blue", "Purple"]
+array = ["Red", "Blue", "Green"]
 hash = {
 	"Hugh Jackman" => "Wolverine",
 	"Ryan Reynolds" => "Deadpool",
 	"Tobey Maguire" => "Spiderman"
 }
 
-# puts "Array values:"
-# array.each do |color|
-# 	puts color
-# 	color.next
-# end
-
-array.map! do |color|
-	puts color
-	color.swapcase
+puts "Array values:"
+array.each do |color|
+	color.next
 end
 
-# puts "Hash values:"
-# hash.each do |actor, role|
-# 	puts "#{actor} is best known for his role as #{role}"
-# end
+array.map! do |letter|
+	puts letter
+	letter.swapcase
+end
 
-# hash.map do
-# end
+p array
 
-
-
-
-# Demonstrate that you can iterate through each one using .each, and then using .map!
-# (modifying the data in some way). Note that you can't call .map! on a hash, so you
-# can skip that (.map returns an array, so a destructive map method is logically impossible
-# on a hash). Print the data structures before and after each call to demonstrate whether
-# they have been modified or not.
+puts "Hash values:"
+hash.each do |actor, role|
+	puts "#{actor} is best known for his role as #{role}"
+end
