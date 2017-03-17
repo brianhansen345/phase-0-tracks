975c73bdec54267c63dac3917ed3d493a6691051
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-17 16:32:01 -0700

    Complete Assignment

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -17,5 +17,9 @@ highway = {
 }
 
 p highway[:first_car][:car_type]
-highway[:third_car][:car_type].push("lorrie")
+highway[:third_car][:car_type] = ("lorrie")
 p highway[:third_car][:car_type]
+highway[:second_car][:people].push("Jimmy")
+p highway[:second_car][:people]
+highway[:third_car][:people].upcase!
+p highway[:third_car][:people]
