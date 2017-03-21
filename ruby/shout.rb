b345ee61da1861facc4d73988bac34f7b82b8db5
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-21 16:29:21 -0700

    Complete Assignment

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,39 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("I hate bats")
+# p Shout.yelling_happily("I love bats")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-  	words + "!!!" + " :)"
-  end
+	def yell_angrily(words)
+		puts "#{words} !!! :("
+	end
+
+	def yell_happily(words)
+		puts "#{words} !!! :)"
+	end
 end
 
-p Shout.yell_angrily("I hate bats")
-p Shout.yelling_happily("I love bats")
+class Man
+	include Shout
+end
 
- 
+class Bear
+	include Shout
+end
+
+man = Man.new
+man.yell_angrily("I hate bears")
+man.yell_happily("I love cats")
+
+bear = Bear.new
+bear.yell_happily("I love men")
+bear.yell_angrily("I hate rats")
+
+
