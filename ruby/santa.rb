5dbd2fbaf7b486674f89dc9ea91f656eb3f5cdc8
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-21 15:24:25 -0700

    Add Release 0-3

---
+++ ruby/santa.rb
@@ -0,0 +1,58 @@
+class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
+# Refactor your code with attr_reader and attr_accessor. Verify that your
+# driver code still works.
+
+	def initialize (gender, ethnicity)
+		p "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		p "That was a good #{cookie_type}!"
+	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.insert(-1, reindeer_name)  
+	end
+
+end
+
+jimbo = Santa.new("male", "white")
+jimbo.speak
+jimbo.eat_milk_and_cookies("Sugar Cookie")
+jimbo.celebrate_birthday
+jimbo.get_mad_at("Vixen")
+jimbo.gender="all genders, simultaneously"
+p jimbo
+p jimbo.age
+p jimbo.ethnicity
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do
+#   santas << Santa.new(example_genders[rand(7)], example_ethnicities[rand(7)])
+# end
+
+# p santas
+
+
+
+
+
+
