3f78d432d9e76c8a9938e59f9c8be0272d8277f8
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-21 18:24:33 -0700

    Add Puppy Class and Call Methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,39 @@
 class Puppy
 
+  def initialze
+  	p "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(integer)
+  	integer.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	p "Rolls over."
+  end
+
+  def dog_years(integer)
+  	p integer * 7
+  end
+
+  def wag_tail
+  	p "Swish swish."
+  end
+
 end
+
+Nala = Puppy.new
+Nala.fetch("ball")
+Nala.speak(2)
+Nala.roll_over
+Nala.dog_years(4)
+Nala.wag_tail
+
+
+
 
