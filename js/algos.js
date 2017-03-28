008544a879d4a988651bc5e089ef4f69a3c7b8cf
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-28 14:37:32 -0700

    Complete Release 0

--- js/algos.js
+++ js/algos.js
@@ -9,34 +9,32 @@ phrases = [
 	"longest phrase",
 	"longer phrase"
 ]
-phrases[0].length
 
 var returnLongest = function (array) {
 	var longest = array[0];
 
 	for (var i=0; i<array.length; i++) {
-		if array[i] > biggest
+		if (array[i] > longest) {
+			longest = array[i];
+		}
 	}
+	console.log(longest);
 }
 
+returnLongest(phrases);
 
 
-
-In algos.js, write a function that takes an array of words or phrases and returns the longest word or
-phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer
-phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason
-through this based on what you already know, rather than Googling the fanciest solution or looking up
-built-in sorting functions. Note that "some string".length will do what you expect in JS.
-Add driver code that tests your function on a few arrays.
-
-var max = function(array) {
-  var biggest = array[0];
+// var max = function(array) {
+//   var biggest = array[0];
   
-  for (var i=0; i < array.length; i++) {
-    if (array[i] > biggest) {
-      biggest = array[i];
-    }
-  }
+//   for (var i=0; i < array.length; i++) {
+//     if (array[i] > biggest) {
+//       biggest = array[i];
+//     }
+//   }
   
-  return biggest;
-};
+//   return biggest;
+// };
+
+
+
