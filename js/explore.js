77bef44af9cfde3d155ccf4e84aeecf42ac0f952
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-03-28 10:51:19 -0700

    Complete Assignment

--- js/explore.js
+++ js/explore.js
@@ -2,25 +2,15 @@
 // Function inputs a string and outputs each of the characters in the reverse order. If a character is at
 // index 0, it moves to the last index.
 
-// Need the index of each letter to change per the following:
-// str[i] = str[str.length - i - 1
-
 var str = "hello";
 
-for (var i = 0; i < str.length; i++) {
-	console.log(str[str.length - i - 1]);
+var reverse = function(str) {
+	for (var i = 0; i < str.length; i++) {
+		console.log(str[str.length - i - 1]);
+	};
+	if (1 === 1) {
+		console.log(str);
+	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+reverse(str)
