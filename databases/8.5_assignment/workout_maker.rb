d666c3161e8d1b4af96a3058ca37f8b25db1133c
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-04-04 12:42:15 -0700

    Add Workouts Table

--- databases/8.5_assignment/workout_maker.rb
+++ databases/8.5_assignment/workout_maker.rb
@@ -1,1 +1,18 @@
 require 'sqlite3'
+
+db = SQLite3::Database.new("workouts.db")
+
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS lifts(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		weight INT,
+		sets INT,
+		reps INT,
+		rest_between_sets INT,
+		able_to_complete BOOLEAN,
+		lift_date INT
+	)
+SQL
+
+db.execute(create_table_cmd)
