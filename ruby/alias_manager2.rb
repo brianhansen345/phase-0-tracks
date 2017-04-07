f08f60f339037b059b91ea20654617e30b1dab8c
brian.l.hansen@gmail.com <brianhansen@MacBook-Pro.local>
2017-04-07 12:35:51 -0700

    Complete Assignment

--- ruby/alias_manager2.rb
+++ ruby/alias_manager2.rb
@@ -1,19 +1,46 @@
-"What's your real name?"
-real_name = gets.chomp
+# BUSINESS LOGIC
 
-name = "Felicia Torres"
+def order_switcher(string)
+	array = []
+	string = string.split(' ')
+	array = array.push(string[1]).push(' ').push(string[0]).join
+	return array
+end
 
-def name_changer(string)
-	string = string.split
+def letter_switcher(string)
+  	string = string.split('')
+  	string.map! do |letter|
+      vowels = "aeiouaAEIOUA"
+      consonants = "bcdfghjklmnpqrstvwxyzbBCDFGHJKLMNPQRSTVWXYZ"
+  	if vowels.include?(letter) == true
+  	    vowels[vowels.index(letter) + 1]
+      elsif consonants.include?(letter) == true
+        consonants[consonants.index(letter) + 1]
+      else
+        letter
+      end
+  	end
+  	string = string.join
+  	return string
+end
 
+def alias_maker(string)
+	order_switcher(string)
+	letter_switcher(string)
+end
 
+# USER INTERFACE
 
-
-
-In phase-0-tracks/ruby/alias_manager.rb, pseudocode and write a method that takes a spy's real name
-(e.g., "Felicia Torres") and creates a fake name with it by doing the following:
-
-Swapping the first and last name.
-Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the
-consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a'
-would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+names = {}
+real_name = ""
+until real_name == 'quit'
+	puts "Enter your real name or type 'quit'."
+	real_name = gets.chomp
+	spy_name = alias_maker(real_name)
+	names[real_name] = spy_name
+	puts "Your spy name is #{spy_name}." unless real_name == 'quit'
+end
+names.delete('quit')
+names.each do |real_name, spy_name|
+	puts "#{spy_name} is actually #{real_name}."
+end
